3411cbe6c15f2303fda946b68e570854d74f378c
Dylan Law <law.dylan.t@gmail.com>
2017-10-31 16:55:07 -0400

    Add framework for phone messages

---
+++ Assets/Scripts/MessageController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MessageController : MonoBehaviour {
+
+	// Exposed variables
+	[ SerializeField ]
+	private Button[] _selectionButtons;
+
+
+	// Messages
+	private void Start() {
+		
+	}
+	
+	private void Update() {
+		
+	}
+}
