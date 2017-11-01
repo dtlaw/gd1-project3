2367d3388b035ea4306482a0742ebf80366a9bc4
Dylan Law <law.dylan.t@gmail.com>
2017-11-01 11:35:27 -0400

    Add scriptableobject for handling chirps

--- Assets/Scripts/MessageController.cs
+++ Assets/Scripts/MessageController.cs
@@ -7,15 +7,38 @@ public class MessageController : MonoBehaviour {
 
 	// Exposed variables
 	[ SerializeField ]
-	private Button[] _selectionButtons;
+	private Button[] _selectionButtons = new Button[ 3 ];
+	[ SerializeField ]
+	private ChirpEvent[] _events;
+
+
+	// Private variables
+	private ChirpEvent _currentEvent;
 
 
 	// Messages
 	private void Start() {
-		
+		LoadEvent( _events[ 0 ]);
 	}
-	
-	private void Update() {
-		
+
+
+
+	// Public interface
+	public void TriggerEvent( int index ) {
+		LoadEvent( _events[ index ]);
+	}
+
+	// TODO: Code for adding a message when a button is clicked
+	public void SelectMessage( int index ) {
+		print( _currentEvent.Options[ index ].FullText );
+	}
+
+
+	// Private interface
+	private void LoadEvent( ChirpEvent e ) {
+		for ( int i = 0; i < _selectionButtons.Length; ++i ) {
+			_currentEvent = e;
+			_selectionButtons[ i ].GetComponentInChildren< Text >().text = e.Options[ i ].HashtagText;
+		}
 	}
 }
