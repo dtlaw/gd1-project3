cbf6cc03f120918da68ada28086d495853a8f783
greencoats <greencoats@outlook.com>
2017-11-08 01:03:04 -0500

    New Day Commit

---
+++ Assets/Scripts/Items.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Items : MonoBehaviour {
+
+    //Define all objects
+    public GameObject cup;
+    public GameObject soap;
+    public GameObject towel;
+    public GameObject bath;
+    public GameObject cabinet;
+    public GameObject door;
+    public GameObject floor;
+    public GameObject sink;
+    public GameObject key;
+    public GameObject mirror;
+    public GameObject paperTowel;
+    public GameObject petals;
+    public GameObject rose;
+    public GameObject rug;
+    public GameObject rugSmall;
+    public GameObject showerHead;
+    public GameObject shower;
+    public GameObject table;
+    public GameObject paper;
+    public GameObject toilet;
+    public GameObject rack;
+    public GameObject shelf;
+    public GameObject stack;
+    public GameObject bin;
+    public GameObject window;
+    public GameObject vent;
+
+    public GameObject player;
+    public GameObject hand;
+
+    public GameObject eventSys;
+
+    //Define Variables
+    private float distance;
+    private GameObject close;
+    private int level;
+
+    // Use this for initialization
+    void Start() {
+        //Find where player is up to
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        level = eventSys.GetComponent<EscapePlan>().level;
+
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+        Debug.DrawRay(transform.position, forward, Color.green, 0, false);
+
+        if (Input.GetMouseButtonDown(0)) { 
+            print("click");
+            CheckObject();
+        }
+
+    }
+
+   
+    void CheckObject() {
+        Ray photoLine = new Ray();
+        RaycastHit hit = new RaycastHit();
+        photoLine.origin = transform.position;
+        photoLine.direction = Vector3.forward;
+
+
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+        Debug.DrawRay(transform.position, forward, Color.green, 0, false);
+        //distance = Vector3.Distance(close.transform.position, hand.transform.position);
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 10f)) {
+            //if (hit.transform != null && distance < 2f) {
+            if (hit.transform != null) {
+                if (hit.transform.tag == "Bath") {
+                    print("HELL YEAH");
+                    close = bath;
+                //} else if (hit.transform.tag == "cup") {
+                //    close = cup;
+                    
+                //} else if (hit.transform.tag == "soap") {
+                //    close = soap;
+                } else if (hit.transform.tag == "Towel") {
+                    close = towel;
+                    print("this is not a cup");
+                //} else if (hit.transform.tag == "cabinet") {
+                //    close = cabinet;
+                } else if (hit.transform.tag == "door") {
+                    close = door;
+                    print("door");
+                //} else if (hit.transform.tag == "floor") {
+                //    close = floor;
+                //} else if (hit.transform.tag == "sink") {
+                //    close = sink;
+                } else if (hit.transform.tag == "key") {
+                    close = key;
+                } else if (hit.transform.tag == "petals") {
+                    close = petals;
+                } else if (hit.transform.tag == "shower") {
+                    close = shower;
+                } else if (hit.transform.tag == "table") {
+                    close = table;
+                } else if (hit.transform.tag == "Vent") {
+                    close = vent;
+                    print("vent");
+                //} else if (hit.transform.tag == "toilet") {
+                //    close = toilet;
+                //} else if (hit.transform.tag == "rack") {
+                //    close = rack;
+                //} else if (hit.transform.tag == "shelf") {
+                //    close = shelf;
+                //} else if (hit.transform.tag == "plate") {
+                //    close = plate;
+                } else if (hit.transform.tag == "bin") {
+                    close = bin;
+                } else if (hit.transform.tag == "window") {
+                    close = window;
+                } 
+            }
+        }
+    }
+}
+
+
+
+        
